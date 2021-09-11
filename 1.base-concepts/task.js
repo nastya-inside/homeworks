254dcb92718e0027f4840619c7ef1e8b49586184
Nastya Grigorenko <oxmurel@gmail.com>
2021-09-11 15:35:58 +0400

    task.js added

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,33 @@
+"use strict";
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = {};
+    
+  if ((a || a == 0) && (b || b == 0) && (c || c == 0)) {
+    const D = b**2-4*a*c;
+    let x1;
+    let x2;
+    if (D === 0) {
+        arr = -b/(2*a);
+        console.log(arr);
+    }
+      else if (D < 0) {
+       console.log (arr);
+      }
+      else if (D > 0) {
+          arr = (-b + Math.sqrt(d) )/(2a);
+          console.log(arr);
+      }
+      else if (D > 0) {
+          arr = (-b - Math.sqrt(d) )/(2a);
+          console.log(arr);
+      }
+  }
   return arr; // array
 }
+
+ 
+
+
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
