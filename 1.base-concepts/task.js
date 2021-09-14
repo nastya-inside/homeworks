94fa5cbaa7792e4e738cc34f6389e4096864b0a7
Nastya Grigorenko <oxmurel@gmail.com>
2021-09-14 19:28:17 +0400

    task.js was changed

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,28 +2,28 @@
 
 function solveEquation(a, b, c) {
 let arr = [];
-let arr1 = [];
     
   if ((a || a == 0) && (b || b == 0) && (c || c == 0)) {
     const D = b**2-4*a*c;
-    let x1;
-    let x2;
     if (D === 0) {
         arr = -b/(2*a);
         console.log(arr);
     }
       else if (D > 0) {
-          arr = (-b + Math.sqrt(d) )/(2a);
-          arr1 = (-b - Math.sqrt(d) )/(2a);
-          console.log(arr, arr1);
+          arr = (-b + Math.sqrt(d))/(2*a);
+          console.log(arr);
+          arr.push(-b - Math.sqrt(d))/(2*a);
+          console.log(arr);
       }
   }
   return arr; // array
 }
 
+solveEquation();
 
 
 
+/*
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
